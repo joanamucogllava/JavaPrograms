46258f6a75ca08870dc57ba0a0e2d98883a0fbb3
Joana Mucogllava <40967988+joanamucogllava@users.noreply.github.com>
2018-07-10 06:22:09 -0700

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  Author: Joana Mucogllava
+  This program is created to change the temperature from Celcius degree to Fahrenheit.
  */
 package temperature;
 
