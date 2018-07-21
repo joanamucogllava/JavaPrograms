9ae772f683a96bbe0f202e6c0212e887ca1ecbfd
Joana Mucogllava <40967988+joanamucogllava@users.noreply.github.com>
2018-07-20 19:55:09 -0700

    new file

---
+++ Replace.java
@@ -0,0 +1,19 @@
+
+
+/**
+ *
+ * @author mucog
+ */
+public class Replace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int first = 7;
+        int second = 19;
+        
+        System.out.println("first=" + first + "second=" + second);
+    }
+    
+}
