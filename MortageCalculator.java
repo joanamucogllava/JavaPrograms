b34cd9b1dd3ff862e3b234aeac8c056382472af5
Joana Mucogllava <mucogllavajoana@hotmail.com>
2022-04-10 15:51:06 -0500

    Create MortageCalculator.java

---
+++ MortageCalculator.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principle: ");
+        int  principle = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float  annualInterestRate = scanner.nextFloat();
+
+        System.out.print("Period(Years): ");
+        int  period = scanner.nextInt();
+
+        float monthlyRate = (annualInterestRate/100/12); // percentage of rate per year
+        int numberPayment = period/12; //number of payments per month in a year
+        double mortage = principle
+                * (monthlyRate * Math.pow(1+monthlyRate, numberPayment))
+                / Math.pow(1+monthlyRate, numberPayment) - 1;
+
+        String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
+
+        System.out.println("Mortage: " + mortageFormatted);
+
+    }
+}
